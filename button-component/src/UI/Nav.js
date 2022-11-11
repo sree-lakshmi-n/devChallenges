b15fd31286aa313bb0db89d2022c56c83a108ca4
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-11 11:00:39 +0530

    Nav Component

--- button-component/src/UI/Nav.js
+++ button-component/src/UI/Nav.js
@@ -1,1 +1,15 @@
 import "./Nav.css";
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <li>Colors</li>
+      <li>Typography</li>
+      <li>Spaces</li>
+      <li>Buttons</li>
+      <li>Inputs</li>
+      <li>Grid</li>
+    </nav>
+  );
+};
+
+export default Nav;
