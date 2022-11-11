6842a096c1d397d603b1dc29fd480d39d0221dd0
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-11 11:29:08 +0530

    wrapped Nav and a div component in wrapper div

--- button-component/src/App.js
+++ button-component/src/App.js
@@ -2,7 +2,12 @@ import "./App.css";
 import Nav from "./UI/Nav";
 
 function App() {
-  return <Nav></Nav>;
+  return (
+    <div className="wrapper">
+      <Nav></Nav>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
