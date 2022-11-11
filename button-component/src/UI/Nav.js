1fed65d4596c7c6e2079bda37e7187b34c6cf410
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-11 11:16:49 +0530

    added logo

--- button-component/src/UI/Nav.js
+++ button-component/src/UI/Nav.js
@@ -2,6 +2,7 @@ import "./Nav.css";
 const Nav = () => {
   return (
     <nav className="nav">
+      <div className="logo">Devchallenges.io</div>
       <li>Colors</li>
       <li>Typography</li>
       <li>Spaces</li>
